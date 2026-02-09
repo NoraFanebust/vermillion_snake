ea8a2d26fce3637a7342d19a0c8b86de6b21086b
NoraFanebust <nora.fanebust@hotmail.com>
2026-02-09 13:21:18 +0100

    change cex

--- iris.R
+++ iris.R
@@ -4,7 +4,8 @@ plot(
   y = iris$Petal.Width,
   xlab = "Petal length",
   ylab = "Petal width",
-  cex = 3,
+  cex = 2,
   pch=21,
-  col = "purple"
+  col = "purple",
+  main = "Iris petal length agianst with"
 )
